00e08490c06db8f01ad673aeb6931ab6e9f057c9
Partha Protim Gogoi <176602154+g-partha@users.noreply.github.com>
2024-10-27 22:38:10 +0530

    Edit removeFromArray.js, removeFromArray.spec.js
    Edit removeFromArray.js, removeFromArray.spec.js to pass the tests.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(myArray, ...items) {
+    const newArray = [];
+    myArray.forEach(
+        (item) => {
+            if(!(items.includes(item))){
+                newArray.push(item);
+            }
+        }
+    )
+    return newArray;
 };
 
 // Do not edit below this line
