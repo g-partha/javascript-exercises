f03a172339d4de443cdd413bc5db5a11fb1080ac
Partha Protim Gogoi <176602154+g-partha@users.noreply.github.com>
2024-10-29 19:57:27 +0530

    Edit tempConversion.js and tempConversion.spec.js
    Edit tempConversion.js and tempConversion.spec.js to pass the tests.

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  let tempC = (temp - 32) * (5 / 9);
+  let formattedTempC = (Math.round(tempC * 10)) / 10;
+  return formattedTempC;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  let tempF = temp * (9 / 5) + 32;
+  let formattedTempF = (Math.round(tempF * 10)) / 10;
+  return formattedTempF;
 };
 
 // Do not edit below this line
