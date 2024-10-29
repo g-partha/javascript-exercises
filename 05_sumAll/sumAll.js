dcfb1ec5bd2adca46645f5060aa0388a14079304
Partha Protim Gogoi <176602154+g-partha@users.noreply.github.com>
2024-10-29 11:24:27 +0530

    Edit sumAll.js and sumAll.spec.js file
    Edit sumAll.js and sumAll.spec.js file pass the tests.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,30 @@
-const sumAll = function() {
+const sumAll = function(initialInteger, finalInteger) {
+    let sum = 0;
+    if(initialInteger < 0 || finalInteger < 0){
+        return "ERROR";
+    }
 
+    else if(!(Number.isInteger(initialInteger)) || !(Number.isInteger(finalInteger)) ){
+        return "ERROR";
+    }
+
+    else if(!(typeof(initialInteger) === "number") || !(typeof(finalInteger) === "number")){
+        return "ERROR";
+    }
+
+    else if(initialInteger > finalInteger){
+        for(let i = initialInteger; i >= finalInteger; i--){
+            sum += i;
+        }
+    }
+
+    else{
+        for(i = initialInteger; i <= finalInteger; i++){
+            sum += i;
+        }
+    }
+    
+    return sum;
 };
 
 // Do not edit below this line
