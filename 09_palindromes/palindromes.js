694ac775eb97422a4441a774df72f065ace233ef
Partha Protim Gogoi <176602154+g-partha@users.noreply.github.com>
2024-10-29 22:01:48 +0530

    Edit palindromes.js and palindromes.spec.js file
    Edit palindromes.js and palindromes.spec.js file to pass the tests.

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,4 +1,11 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    const alphaNumericCharacters = "abcdefghijklmnopqrstuvwxyz0123456789";
+    let lowerString = string.toLowerCase();
+    const stringArray = lowerString.split("");
+    const alphaNumericStringArray = stringArray.filter((character) => alphaNumericCharacters.includes(character));
+    let alphaNumericString = alphaNumericStringArray.join("");
+    let reversedAlphaNumericString = alphaNumericString.split("").reverse().join("");
+    return alphaNumericString == reversedAlphaNumericString;
 
 };
 
