64e30c7891583f17302a1e5cc2a961fab6cbd888
Partha Protim Gogoi <176602154+g-partha@users.noreply.github.com>
2024-10-29 20:18:29 +0530

    Edit calculator.js and calculator.spec.js file
    Edit calculator.js and calculator.spec.js file to pass the tests.

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,47 @@
-const add = function() {
+const add = function(number1, number2) {
+  return number1 + number2;
 	
 };
 
-const subtract = function() {
-	
+const subtract = function(number1, number2) {
+	return number1 - number2;
 };
 
-const sum = function() {
-	
+const sum = function(numberArray) {
+	let sumValue = 0;
+  for(let i = 0; i < numberArray.length; i++){
+    sumValue += numberArray[i];
+  }
+  return sumValue;
 };
 
-const multiply = function() {
-
+const multiply = function(numberArray) {
+	let multipliedValue = 1;
+  for(let i = 0; i < numberArray.length; i++){
+    multipliedValue *= numberArray[i];
+  }
+  return multipliedValue;
 };
 
-const power = function() {
-	
+const power = function(number1, number2) {
+	let requiredValue = 1;
+  for(let i = 0; i < number2; i++){
+    requiredValue *= number1;
+  }
+  return requiredValue;
 };
 
-const factorial = function() {
-	
+const factorial = function(number) {
+	if(number == 0){
+    return 1;
+  }
+  else{
+    let requiredValue = 1;
+    for(let i = 1; i <= number; i++){
+      requiredValue *= i;
+    }
+    return requiredValue;
+  }
 };
 
 // Do not edit below this line
