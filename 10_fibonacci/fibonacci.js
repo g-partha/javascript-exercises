746fd39b1fdd595a5e140af59e3b1c5e2614b511
Partha Protim Gogoi <176602154+g-partha@users.noreply.github.com>
2024-10-30 22:04:27 +0530

    Edit fibonacci.js and fibonacci.spec.js file
    Edit fibonacci.js and fibonacci.spec.js file to pass the tests.

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,23 @@
-const fibonacci = function() {
+const fibonacci = function(placeOfNumberParameter) {
+    let placeOfNumber = +placeOfNumberParameter;
+    let fib = 1;
+    let fib_1 = 0;
+    if(placeOfNumber == 0){
+        return 0;
+    }
+    else if(placeOfNumber < 0){
+        return "OOPS";
+    }
 
+    else{
+        for(let i = 0; i < placeOfNumber; i++){
+        if (i == 0){continue;}
+        let fib_2 = fib_1;
+        fib_1 = fib;
+        fib = fib_1 + fib_2;
+        }
+        return fib;
+    }
 };
 
 // Do not edit below this line
