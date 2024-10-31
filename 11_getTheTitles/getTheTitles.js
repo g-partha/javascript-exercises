2776a9ed80f21bd0d652aba1944ac4ec721a1427
Partha Protim Gogoi <176602154+g-partha@users.noreply.github.com>
2024-10-31 18:40:03 +0530

    Edit getTheTitles.js and getTheTitles.spec.js file
    Edit getTheTitles.js and getTheTitles.spec.js file to pass the tests.

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,9 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(books) {
+    const titleArray = [];
+    for(i = 0; i < books.length; i++){
+        titleArray.push(books[i].title);
+    }
+    return titleArray;
 };
 
 // Do not edit below this line
