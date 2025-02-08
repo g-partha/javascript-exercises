e07ecf4570b60f0f83bfb2a16fab327d7da6ce3f
Partha Protim Gogoi <176602154+g-partha@users.noreply.github.com>
2025-02-08 16:09:27 +0000

    Edit sumAll to pass the tests

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
-
+const sumAll = function(number1, number2) {
+    let result = 0;
+    if(!(typeof number1 == "number" && typeof number2 == "number")){
+        return "ERROR";
+    } else if(number1 < 0 || number2 < 0){
+        return "ERROR";
+    } else if(!(Number.isInteger(number1) && Number.isInteger(number2))){
+        return "ERROR";
+    } else if(number1 < number2){
+        for(let i = number1; i <= number2; i++) result += i;
+    } else {
+        for(let i = number1; i >= number2; i--) result += i;
+    }
+    return result;
 };
 
 // Do not edit below this line
