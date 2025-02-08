2f2dcd46427c8d68d02cfe8fd28e8fd2737dc547
Partha Protim Gogoi <176602154+g-partha@users.noreply.github.com>
2025-02-08 15:50:01 +0000

    Edit removeFromArray to pass the tests

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,9 @@
 const removeFromArray = function(array, ...args) {
+    const result = [];
+    for(item of array){
+        if(!(args.includes(item))) result.push(item);
+    }
+    return result;
 };
 
 // Do not edit below this line
